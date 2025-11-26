55d29fc57f4d71067f383c08c55fb9511c8326ff
rezvaya <rezvaya2016@outlook.com>
2025-11-26 20:02:17 +0300

    task 1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello")
+"""
+Постройте график параболы y=x^2 для x от -10 до 10. Добавьте заголовок, подписи осей и сетку.
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-10, 10, 200)
+y = x ** 2
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+plt.show()
